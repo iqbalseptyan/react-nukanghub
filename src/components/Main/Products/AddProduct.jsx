5d3db6bc109146bd85da85c8f28ba9a124f55e74
Iqbal Septyan <iqbalseptyan@gmail.com>
2021-08-31 16:48:31 +0700

    update toolbar deskripsi produk

--- src/components/Main/Products/AddProduct.jsx
+++ src/components/Main/Products/AddProduct.jsx
@@ -16,19 +16,25 @@ const AddProduct = () => {
           <CKEditor
             editor={ClassicEditor}
             data=""
-            onReady={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
-            }}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              console.log({ event, editor, data });
-            }}
-            onBlur={(event, editor) => {
-              console.log("Blur.", editor);
-            }}
-            onFocus={(event, editor) => {
-              console.log("Focus.", editor);
+            config={{
+              toolbar: [
+                "heading",
+                "|",
+                "bold",
+                "italic",
+                "blockQuote",
+                "link",
+                "numberedList",
+                "bulletedList",
+                "insertTable",
+                "tableColumn",
+                "tableRow",
+                "mergeTableCells",
+                "mediaEmbed",
+                "|",
+                "undo",
+                "redo",
+              ],
             }}
           />
         </div>
