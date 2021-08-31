8da4a072ef2641ffd78c020cf0731256bab8b69a
Iqbal Septyan <iqbalseptyan@gmail.com>
2021-08-31 16:41:09 +0700

    add products 2

--- src/components/Main/Products/AddProduct.jsx
+++ src/components/Main/Products/AddProduct.jsx
@@ -1,10 +1,84 @@
-import React from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const AddProduct = () => {
   return (
-    <>
-      <h1>Add Prodcut</h1>
-    </>
+    <Row>
+      <Col lg="8">
+        <Form.Group controlId="formNamaProduk">
+          <Form.Label>Nama Produk</Form.Label>
+          <Form.Control type="text" placeholder="Nama produk" />
+        </Form.Group>
+
+        <div style={{ maxWidth: "800px" }}>
+          <Form.Label>Deskripsi Produk</Form.Label>
+          <CKEditor
+            editor={ClassicEditor}
+            data=""
+            onReady={(editor) => {
+              // You can store the "editor" and use when it is needed.
+              console.log("Editor is ready to use!", editor);
+            }}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              console.log({ event, editor, data });
+            }}
+            onBlur={(event, editor) => {
+              console.log("Blur.", editor);
+            }}
+            onFocus={(event, editor) => {
+              console.log("Focus.", editor);
+            }}
+          />
+        </div>
+
+        <Form.Group controlId="formNamaProduk">
+          <Form.Label>Gambar Produk</Form.Label>
+          <Form.Control type="file" placeholder="Nama produk" />
+        </Form.Group>
+      </Col>
+
+      <Col lg="4">
+        <Form.Group controlId="formHarga">
+          <Form.Label>Harga</Form.Label>
+          <Form.Control type="number" placeholder="Harga" />
+        </Form.Group>
+
+        <Form.Group controlId="formHargaJual">
+          <Form.Label>Harga Jual</Form.Label>
+          <Form.Control type="number" placeholder="Harga jual" />
+        </Form.Group>
+
+        <Form.Group controlId="formProdukStok">
+          <Form.Label>Produk Stok</Form.Label>
+          <Form.Control type="number" placeholder="Produk stok" />
+        </Form.Group>
+
+        <Form.Group controlId="formKategori">
+          <Form.Label>Kategori</Form.Label>
+          <Form.Control as="select">
+            <option>Pilih Kategori</option>
+            <option>Batako</option>
+            <option>Batu</option>
+            <option>Pasir</option>
+            <option>Furnitur</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formLokasi">
+          <Form.Label>Lokasi</Form.Label>
+          <Form.Control as="select">
+            <option>Pilih Lokasi</option>
+            <option>Malang</option>
+            <option>Blitar</option>
+            <option>Tulungagung</option>
+            <option>Kediri</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="outline-primary">Publish Produk</Button>
+      </Col>
+    </Row>
   );
 };
 
--- src/components/Main/Products/Product.jsx
+++ src/components/Main/Products/Product.jsx
@@ -1,27 +1,18 @@
-// import { Tab } from "bootstrap";
-import React, { useState } from "react";
+import React from "react";
 import { Tabs, Tab } from "react-bootstrap";
 import AddProduct from "./AddProduct";
 import TableProduct from "./TableProduct";
 
 const Product = () => {
-  const [key, setKey] = useState("product");
   return (
-    <>
-      <h1>Product</h1>
-      <Tabs
-        id="controlled-tab-example"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-      >
-        <Tab eventKey="product" title="Product">
-          <TableProduct />
-        </Tab>
-        <Tab eventKey="profile" title="Add Product">
-          <AddProduct />
-        </Tab>
-      </Tabs>
-    </>
+    <Tabs id="controlled-tab-example">
+      <Tab eventKey="product" title="Product">
+        <TableProduct />
+      </Tab>
+      <Tab eventKey="profile" title="Add Product">
+        <AddProduct />
+      </Tab>
+    </Tabs>
   );
 };
 
